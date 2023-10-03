c8f1e6edfcf8cf479f1bc013eea68d72ae73c1ee
Shrinidhi Hegde <shrinidhi.hegde@capgemini.com>
2023-10-03 10:37:46 +0530

    Initial Commit

---
+++ gitTemp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitTemp
+//
+//  Created by Shrinidhi Hegde on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitTemp/gitTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTempApp.swift
+//  gitTemp
+//
+//  Created by Shrinidhi Hegde on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct gitTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
